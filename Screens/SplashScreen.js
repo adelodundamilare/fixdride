0607b38b0c66fdfa5cf447492ab65f643491ce04
Damilare <adelodundamilare@yahoo.com>
2019-03-03 02:13:27 +0100

    added splash screen

---
+++ Screens/SplashScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { 
+  StyleSheet, Image, View 
+} from 'react-native';
+
+class SplashScreen extends Component {
+
+  componentDidMount() {
+    setTimeout(()=>{
+      // go to next screen
+      this.props.navigation.navigate('LoginScreen')
+    }, 2500)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('../assets/logo.png')} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {width:250, resizeMode:'contain'}
+});
+
+export default SplashScreen
